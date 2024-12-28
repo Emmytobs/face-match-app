e203bf5cba7f1f24e840b021ba5148d6187926c4
Emmanuel Otobo <eotobo11@gmail.com>
2024-12-28 19:48:22 -0330

    Changed the fast-api version to fix Pydantic bug with AWS Lambda

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import boto3
+import uvicorn
 from typing import Annotated
 from fastapi import FastAPI, File, HTTPException
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from mangum import Mangum
 
 app = FastAPI()
+"""
+  The "/static" path is used to serve the external CSS and JavaScript files used by the index.html file
+"""
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+handler = Mangum(app) # Handler for AWS Lambda
+
 rekognition_client = boto3.client('rekognition')
 
 @app.get("/")
@@ -20,10 +31,6 @@ def upload_image(
   detect_faces_result = rekognition_client.detect_faces(Image=photo_id)
   if len(detect_faces_result['FaceDetails']) > 1:
     raise HTTPException(status_code=422, detail="It seems the photo ID contains more than one face or no faces at all. Please upload a photo ID with exactly one face")
-  
-  # # face_details = detect_faces_result['FaceDetails'][0]
-  # # face_details['Confidence']
-  # pprint("detect_faces_result: ", detect_faces_result)
 
   headshot = { "Bytes": uploaded_headshot }
   compare_faces_result = rekognition_client.compare_faces(
@@ -31,12 +38,15 @@ def upload_image(
     TargetImage=headshot
   )
   [face_match] = compare_faces_result['FaceMatches']
+  
+  # Only accept the face match if the similarity is greater than 90
   if face_match['Similarity'] < 90:
     raise HTTPException(status_code=422, detail="We could not verify your identity. Please try again with a different photo ID or headshot")
   return face_match
-  # print("compare_faces_result: ", compare_faces_result)
 
 @app.get("/hello")
 def hello_world():
   return { "details": "Hello World! Connected to server successfully" }
 
+if __name__ == "__main__":
+  uvicorn.run("main:app", port=8000, log_level="info", reload=True)
