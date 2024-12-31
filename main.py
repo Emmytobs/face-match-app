2b3aa5dc18e61f05e50e8614b4d496338f9b5a77
Emmanuel Otobo <eotobo11@gmail.com>
2024-12-31 15:34:17 -0330

    updated main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from typing import Annotated
 from fastapi import FastAPI, File, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-
 from mangum import Mangum
 
 app = FastAPI()
 """
-  The "/static" path is used to serve the external CSS and JavaScript files used by the index.html file
+  The "/static" path is used to serve the external CSS and JavaScript files used by the index.html file.
+  Note: When you make changes to files in the /static directory, you'll need to comment out the line below, restart the server, and add the line back in.
 """
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -23,26 +23,26 @@ async def read_index():
 
 @app.post('/upload-image')
 def upload_image(
-  uploaded_photo_id: Annotated[bytes, File()], 
-  uploaded_headshot: Annotated[bytes, File()]
+  uploaded_face_pic: Annotated[bytes, File()], 
+  live_headshot: Annotated[bytes, File()]
 ):
-  photo_id = {"Bytes": uploaded_photo_id}
-  # Validate to make sure there's exactly one face in the uploaded photo_id
-  detect_faces_result = rekognition_client.detect_faces(Image=photo_id)
-  if len(detect_faces_result['FaceDetails']) > 1:
-    raise HTTPException(status_code=422, detail="It seems the photo ID contains more than one face or no faces at all. Please upload a photo ID with exactly one face")
+  try:
+    source_image = {"Bytes": uploaded_face_pic}
+    # Validate to make sure there's exactly one face in the uploaded_face_pic
+    detect_faces_result = rekognition_client.detect_faces(Image=source_image)
+    if len(detect_faces_result['FaceDetails']) > 1:
+      raise HTTPException(status_code=422, detail="It seems the uploaded face pic contains more than one face or no faces at all. Please upload a photo with exactly one face")
 
-  headshot = { "Bytes": uploaded_headshot }
-  compare_faces_result = rekognition_client.compare_faces(
-    SourceImage=photo_id,
-    TargetImage=headshot
-  )
-  [face_match] = compare_faces_result['FaceMatches']
-  
-  # Only accept the face match if the similarity is greater than 90
-  if face_match['Similarity'] < 90:
-    raise HTTPException(status_code=422, detail="We could not verify your identity. Please try again with a different photo ID or headshot")
-  return face_match
+    target_image = { "Bytes": live_headshot }
+    compare_faces_result = rekognition_client.compare_faces(
+      SourceImage=source_image,
+      TargetImage=target_image,
+      SimilarityThreshold=0
+    )
+    [face_match] = compare_faces_result['FaceMatches']
+    return face_match
+  except Exception as e:
+    raise e
 
 @app.get("/hello")
 def hello_world():
